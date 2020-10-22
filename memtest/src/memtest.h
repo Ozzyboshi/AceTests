d50b879bbc00fce34198447f608502949118dcd6
Ozzyboshi <gun101@email.it>
2020-10-22 10:12:12 +0000

    added memtest

---
+++ memtest/src/memtest.h
@@ -0,0 +1,13 @@
+#ifndef _memtest_H_
+#define _memtest_H_
+
+// Function headers from game.c go here
+// It's best to put here only those functions which are needed in other files.
+
+void memtestGsCreate(void);
+
+void memtestGsLoop(void);
+
+void memtestGsDestroy(void);
+
+#endif // _
